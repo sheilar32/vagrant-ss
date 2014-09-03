93a8d9d32ee9d0c383e3dd6ec2975ba43ba1d540
Jerald Sheets <questy@gmail.com>
2014-09-03 14:14:02 -0400

    added directory environments configuration

--- master.sh
+++ master.sh
@@ -4,6 +4,7 @@
 /opt/puppet/bin/puppet module install zack/r10k
 /sbin/service iptables stop
 /bin/mkdir -p /etc/puppetlabs/puppet/environments
+/bin/mkdir -p /etc/puppetlabs/puppet/hiera
 
 # Place the r10k configuration file
 cat > /var/tmp/configure_r10k.pp << 'EOF'
@@ -19,6 +20,37 @@ class { 'r10k':
   purgedirs         => ["${::settings::confdir}/environments"],
   manage_modulepath => false,
 }
+EOF
+
+# Place the directory environments config file
+cat > /var/tmp/configure_directory_environments.pp << 'EOF'
+
+######                           ######
+##  Configure Directory Environments ##
+######                           ######
+
+##  This manifest requires the puppetlabs/inifile module and will attempt to
+##  configure puppet.conf according to the blog post on using R10k and
+##  directory environments.  Beware!
+
+# Default for ini_setting resources:
+Ini_setting {
+  ensure => present,
+  path   => "${::settings::confdir}/puppet.conf",
+}
+
+ini_setting { 'Configure environmentpath':
+  section => 'main',
+  setting => 'environmentpath',
+  value   => '$confdir/environments',
+}
+
+ini_setting { 'Configure basemodulepath':
+  section => 'main',
+  setting => 'basemodulepath',
+  value   => '$confdir/modules:/opt/puppet/share/puppet/modules',
+}
+
 EOF
 
 # Now, apply your new configuration
